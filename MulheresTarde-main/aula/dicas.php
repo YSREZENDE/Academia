b7ff1d5f743b5a29e5dc7550b929dc3dac9245f2
YSREZENDE <107312818+YSREZENDE@users.noreply.github.com>
2022-11-23 16:54:48 -0300

    base

---
+++ MulheresTarde-main/aula/dicas.php
@@ -0,0 +1,87 @@
+<?php
+require_once 'head.php';
+require_once 'menu.php';
+
+
+?>
+
+    <h1> Dicas de treino</h1>
+
+ <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-12 text-center">
+              <img src="imagens\musculacao_1_widexl.jpg" class="img-fluid">
+          </div>
+
+      </div>
+  </div>
+
+  <div class="container-fluid texto">
+    <div class="row">
+      <div class="col-md-12 text-center">
+  <h2>Treino ideal pra você.</h2>
+
+  <p>Lorem ipsum dolor sit amet. Sed maiores accusantium 33 velit consectetur est minus unde. Et earum soluta qui veritatis modi hic modi obcaecati est magnam quibusdam et recusandae odio At saepe quisquam ut quasi autem. Est distinctio temporibus sed dolores repellendus in deserunt dolorem ex dicta nulla eum autem quas et quibusdam fuga et quia laborum. </p>
+      </div>
+    </div>
+
+
+  <div class="container-fluid imagens">
+    <div class="row">
+        <div class="col-md-4 ">
+            <img src="imagens\musculacao_1_widexl.jpg" class="img-fluid">
+        </div>
+
+        <div class="col-md-4">
+            <img src="imagens\musculacao_1_widexl.jpg" class="img-fluid">
+        </div>
+
+      
+
+    </div>
+</div>
+<?php
+require_once 'footer.php'
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
